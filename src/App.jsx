608cc48dbb4b6b0e3645bb764b84c98ca65264b5
O. Eugenio <ome18@fsu.edu>
2024-03-31 02:42:10 -0400

    Added the ability for the site to work in mobile browsers more easier.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import logo from '/logo.png'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import logo from '/logo.png'; // Assuming logo.png is in the same directory as this component
+import './App.css';
 import Ticket from "./Ticket.jsx";
 import SocialBar from "./SocialBar.jsx";
 
 function App() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  return (
-    <>
-        <img src={logo} className="logo" alt="Vite logo" />
-        <Ticket/>
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
 
-        <a className="signup" href="https://docs.google.com/forms/u/1/d/e/1FAIpQLSc80vg5aTbhFolYtFCpKQ6sm-Y06Yt7vvxct3NSCVjvsPZXug/viewform?usp=send_form">Sign up now to be a fighter... Click here!</a>
-        <SocialBar/>
+        window.addEventListener('resize', handleResize);
 
-    </>
-  )
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const showImage = windowWidth <= 800;
+
+    return (
+        <>
+            <img src={logo} className="logo" alt="Vite logo" />
+            <br />
+            {showImage && <img className="main_image" src="/CFL_AD.png" alt="CFL Advertisement" />}
+
+            <div className="center_media_container">
+                <Ticket />
+            </div>
+            <SocialBar/>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/SocialBar.jsx
+++ src/SocialBar.jsx
@@ -4,9 +4,9 @@ import './SocialBar.css'
 const SocialBar = () => {
     return (
         <div className="social-bar">
-            <p>Follow us on social media!</p>
             <div className="social-menu">
                 <ul>
+                    <p>Follow us on social media!</p>
                     <li><a href="https://twitter.com/CFL_FightLeague" target="blank"><i className="fab fa-twitter"></i></a>
                     </li>
                     <li><a href="https://www.instagram.com/creator_fightleague/" target="blank"><i
