d453b6c23bbc5dadfaa3843c0dc8fb0482c9cf3b
O. Eugenio <ome18@fsu.edu>
2024-05-01 12:39:18 -0400

    fixed images

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ import { Button, Navbar } from "flowbite-react";
 const CFL_Navbar = () => {
     return (
         <div className="navbar">
-            <Navbar fluid rounded className="shadow-2xl shadow-blue-600 ">
+            <Navbar fluid rounded className="shadow-2xl shadow-blue-600 bg-white">
                 <Navbar.Brand href="https://flowbite-react.com" className="">
                     <img src="/logo.png" className="mr-3 h-6 sm:h-9 invert" alt="Flowbite React Logo" />
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-black">Creator Fight League</span>
