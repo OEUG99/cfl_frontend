40dff6c9c169689cae011838b5ff382bf9bef7fa
O. Eugenio <ome18@fsu.edu>
2024-05-01 12:43:51 -0400

    fixed images

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,8 +8,8 @@ const CFL_Navbar = () => {
         <div className="navbar">
             <Navbar fluid rounded className="shadow-2xl shadow-blue-600 bg-white">
                 <Navbar.Brand href="https://flowbite-react.com" className="">
-                    <img src="/logo.png" className="mr-3 h-6 sm:h-9 invert" alt="Flowbite React Logo" />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-black">Creator Fight League</span>
+                    <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Creator Fight League</span>
                 </Navbar.Brand>
                 <div className="my-2">
                     <Navbar.Toggle/>
